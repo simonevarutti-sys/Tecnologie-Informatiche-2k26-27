2127c2b5cd83d5457e741cc5160071587f2a41ba
simonevarutti-sys <simone.varutti@isspilimbergo.it>
2026-09-21 08:42:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+i = 1
+while i <= 100:
+  if i % 2 == 0: # se è pari
+    print(str(i) + "Beep") # stampa beep 
+  else: # altrimenti è dispari 
+    print(str(i) + "Bop") #quindi stampa bop
+  i = i + 1
